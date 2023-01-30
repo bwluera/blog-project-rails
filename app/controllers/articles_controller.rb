86172ca08a999ff5e7d58d8cac44d7b4d00d1eed
ssnd1 <52203468+ssnd1@users.noreply.github.com>
2023-01-30 00:32:28 -0600

    Finish project.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,49 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # GET action of Create.
+  def new
+    @article = Article.new
+  end
+
+  # POST action of Create.
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET action of Update.
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # POST action of Update.
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
+# Simple text-only article.
 class Article < ApplicationRecord
+  include Visible
+
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
